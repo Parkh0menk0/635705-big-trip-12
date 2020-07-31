4ec4cb0a2a5f740c92474726e1f0825f042a6a9a
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-07-31 03:37:15 +0300

    Исправляет ошибки

--- src/view/trip-cost.js
+++ src/view/trip-cost.js
@@ -1,4 +1,4 @@
-const createTripСostTemplate = () => {
+export const createTripСostTemplate = () => {
   return (
     `<p class="trip-info__cost">
       Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
