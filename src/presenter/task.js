816ea20b6364d96816e05cbf088895aaedc4cff9
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-09-08 21:42:23 +0300

    Добавляет возможность переиспользования

--- src/presenter/task.js
+++ src/presenter/task.js
@@ -1,6 +1,6 @@
 import PointEditView from "../view/point-edit.js";
 import PoinView from "../view/poin.js";
-import {render, replace} from "../utils/render.js";
+import {render, replace, remove} from "../utils/render.js";
 import {ESC_KEY} from "../const.js";
 
 export default class Point {
@@ -18,13 +18,35 @@ export default class Point {
   init(point) {
     this._point = point;
 
+    const prevPointComponent = this._pointComponent;
+    const prevPointEditComponent = this._pointEditComponent;
+
     this._pointComponent = new PoinView(point);
     this._pointEditComponent = new PointEditView(point);
 
     this._pointComponent.setClickHandler(this._handleEditClick);
     this._pointEditComponent.setFormSubmitHandler(this._handleFormSubmit);
 
-    render(this._pointListContainer, this._pointComponent);
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this._pointListContainer, this._pointComponent);
+      return;
+    }
+
+    if (this._pointListContainer.getElement().contains(prevPointComponent.getElement())) {
+      replace(this._pointComponent, prevPointComponent);
+    }
+
+    if (this._pointListContainer.getElement().contains(prevPointEditComponent.getElement())) {
+      replace(this._pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  }
+
+  destroy() {
+    remove(this._pointComponent);
+    remove(this._pointEditComponent);
   }
 
   _replacePointToForm() {
