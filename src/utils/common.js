6538be8ca1b7e2ec12c766a9ced400b909bbb28b
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-09-23 08:10:14 +0300

    Подключает lodash

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,7 @@
+import {groupBy, range, isEqual, isNumber} from 'lodash';
+
+export {groupBy, range, isEqual, isNumber};
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -11,4 +15,15 @@ export const shuffleArray = (arr) => {
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
+};
+
+export const updateItem = (source, updatedItem) => {
+  const index = source.findIndex((item) => item.id === updatedItem.id);
+
+  if (index < 0) {
+    return source;
+  } else {
+    source[index] = Object.defineProperties({}, Object.getOwnPropertyDescriptors(updatedItem));
+    return source;
+  }
 };
