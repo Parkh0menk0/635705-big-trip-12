e103777f4ea3d85383e5add59f03acbd3f865aeb
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-09-23 09:56:22 +0300

    Рефакторинг представления маршрута

--- src/view/route-info.js
+++ src/view/route-info.js
@@ -1,21 +1,29 @@
 import AbstractView from "./abstract.js";
 
-const createRouteInfoTemplate = () => {
+export const createRouteInfoTemplate = ({route, duration, cost}) => {
   return (
     `<section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+        <h1 class="trip-info__title">${route.join(` &mdash; `)}</h1>
 
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+        <p class="trip-info__dates">${duration}</p>
       </div>
+
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost}</span>
+      </p>
     </section>`
   );
 };
 
 export default class RouteInfo extends AbstractView {
+  constructor(routeInfo) {
+    super();
+    this._routeInfo = routeInfo;
+  }
 
   getTemplate() {
-    return createRouteInfoTemplate();
+    return createRouteInfoTemplate(this._routeInfo);
   }
 
 }
